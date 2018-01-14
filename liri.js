02dd49b932f6888cad38442068c5ec8b7411328c
djsulli24 <djsulli24@gmail.com>
2018-01-14 16:02:05 -0600

    The Spotify API call functionality is complete.

--- liri.js
+++ liri.js
@@ -8,18 +8,32 @@ var client = new Twitter(keys.twitter);
 
 var liri = {
     tweets: function() {
-        var params = {screen_name: 'danielliriapp'};
+        var params = {screen_name: 'oprah'};
         client.get('statuses/user_timeline', params, function(error, tweets, response) {
             if (!error) {
+                console.log("Oprah's Tweets:\n");
                 for (var i = 0; i < 20; i++) {
                     console.log((i+1) + ". " + tweets[i].text + "\n");
                 }
             }
         });
     },
-    song: function() {},
+    song: function(songName) {
+        // If user has entered a songname, the query will be run for that song
+        // Else, it'll query for "The Sign" by Ace of Base
+        spotify.search({ type: 'track', query: (songName)? songName: "The Sign - Ace of Base", limit: 1 }, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            }
+           
+          console.log("Artist: " + data.tracks.items[0].album.artists[0].name); 
+          console.log("Song: " + data.tracks.items[0].name);
+          console.log("Preview Link: " + data.tracks.items[0].preview_url);
+          console.log("Album: " + data.tracks.items[0].album.name);
+          });
+    },
     movie: function() {},
     do: function() {}
 };
 
-liri.tweets();
+liri.song();
