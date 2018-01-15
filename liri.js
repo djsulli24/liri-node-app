d5ef71b35f94b6f8049db10f67d823c2876179c7
djsulli24 <djsulli24@gmail.com>
2018-01-15 09:40:37 -0600

    App is functionally complete, with all comments.

--- liri.js
+++ liri.js
@@ -1,14 +1,24 @@
+/* .env file allows storage of API keys locally, which is in the .gitignore file, so 
+keys cannot be taken from a public repo */
 require("dotenv").config();
+// Holds the variables for all API keys
 var keys = require("./keys");
+// For making API calls to Twitter
 var Twitter = require('twitter');
+// For making API calls to Spotify
 var Spotify = require('node-spotify-api');
+// For making any GET HTTP request (specifically for OMDB API here)
 var request = require('request');
+// For reading and writing to files (specifically log.txt and random.txt here)
 var fs = require("fs");
+// Initializing a new instance of the Spotify and Twitter objects with they keys
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-
+// This object holds all the methods for this program
 var liri = {
+    // Makes an API call to Twitter for Oprah's tweets, and prints the 20 most recent
+    // in the console and logs them to the log.txt file
     tweets: function() {
         var params = {screen_name: 'oprah'};
         client.get('statuses/user_timeline', params, function(error, tweets, response) {
@@ -23,6 +33,8 @@ var liri = {
             }
         });
     },
+    // This makes an API call to Spotify with the song name string and outputs
+    // the song information in the console and to the log.txt file
     song: function(songName) {
         // If user has entered a songname, the query will be run for that song
         // Else, it'll query for "The Sign" by Ace of Base
@@ -43,38 +55,45 @@ var liri = {
     // This function must be passed the parameterized movie title, ex: "back+to+the+future"
     // The parameterize() function does this when it is passed the process.argv array
     movie: function(movieNameParameterized) {
-
-        let queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + movieNameParameterized;
+        // An API call is made to OMDB with the user's entered movie. If they haven't
+        // entered a movie, the call is made for the movie "Mr. Nobody"
+        let queryURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + ((movieNameParameterized)? movieNameParameterized:"Mr+Nobody");
         request(queryURL, function(error, response, body) {
+            let object = JSON.parse(body);
             let string = `-------------------------------
-                \nTitle: ${JSON.parse(body).Title}
-                \nYear: ${JSON.parse(body).Year}
-                \nIMDB Rating: ${JSON.parse(body).imdbRating}
-                \nRotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}
-                \nCountry of Production: ${JSON.parse(body).Country}
-                \nLanguage: ${JSON.parse(body).Language}
-                \nPlot: ${JSON.parse(body).Plot}
-                \nActors: ${JSON.parse(body).Actors}
+                \nTitle: ${object.Title}
+                \nYear: ${object.Year}
+                \nIMDB Rating: ${object.imdbRating}
+                \nRotten Tomatoes Rating: ${object.Ratings[1].Value}
+                \nCountry of Production: ${object.Country}
+                \nLanguage: ${object.Language}
+                \nPlot: ${object.Plot}
+                \nActors: ${object.Actors}
                 \n-------------------------------\n`
             console.log(string);
             liri.logFile(string);
         });
     },
-    do: function(command, string) {
-
-    },
+    // This takes a string ("Back to the Future"), adds + characters in between
+    // words ("Back+to+the+Future") and returns the string
     paramaterize: function(processArgvArray) {
         let array = process.argv;
         array.splice(0,3);
         let string = array.join("+");
         return string;
     },
+    // This takes all of the words the user has added after the command in the CLI
+    // from the process.argv array and combines them into a string with spaces in between
+    // words, returning the string
     stringify: function(processArgvArray) {
         let array = process.argv;
         array.splice(0,3);
         let string = array.join(" ");
         return string;
     },
+    // This function takes the user's command (such as movie-this), and decides which
+    // method to call based on the command, then passes the stringified or parameterized
+    // process.argv as an argument to the correct method
     commandRouting: function(command) {
         if (command === "my-tweets") {
             liri.tweets();
@@ -85,6 +104,16 @@ var liri = {
         else if (command === "movie-this") {
             liri.movie(liri.paramaterize(process.argv));
         }
+        /* FUTURE IMPROVEMENT NEEDED: All of the other methods are built to 
+        take the user's entered data from process.argv, which can't be done
+        for do-what-it-says, since the data comes from the random.txt file.
+        So, this deletes out process.argv[3] and all higher indices, and sets
+        process.argv[3] equal to the string from random.txt. It then calls this
+        commandRouting method recursively with the command. 
+        
+        The commandRouting method and other methods should be refactored to take
+        both a command and a string as arguments. Then process.argv wouldn't need
+        to be directly manipulated*/
         else if (command === "do-what-it-says") {
             fs.readFile("random.txt", "utf8", function(error, data) {
                 let string = data.replace(/"/g, "");
@@ -98,6 +127,7 @@ var liri = {
             console.log("I don't recognize that command.");
         }
     },
+    // This method logs a given string to the log.txt file.
     logFile: function(string) {
         fs.appendFile("log.txt", string, function(err) {
             if (err) {
@@ -106,7 +136,7 @@ var liri = {
         });
     }
 };
-
+// Passes the user's command given on the CLI to the commandRouting method
 liri.commandRouting(process.argv[2]);
 
 
